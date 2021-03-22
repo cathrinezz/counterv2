b926d828809a3606a430f659f2c8fc1c1a05602c
student <o1083016@gm.pu.edu.tw>
2021-03-22 11:50:52 +0800

    20210322 早上

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(){
+    var counter:Int =0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun ADD(v: View){
+        if(v == btn1){
+            counter++
+        }
+        else if(v == btn2){
+            counter +=2
+        }
+        else if(v == btn3)
+        {
+            counter = 0
+
+        }
+        txv.text = counter.toString()
+    }
+}
