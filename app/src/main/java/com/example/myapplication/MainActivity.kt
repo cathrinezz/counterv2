037e4cfbcb9a21de342cb0899d96230d8346aedd
cathrinezz <o1083016@gm.pu.edu.tw>
2021-03-29 11:58:21 +0800

    29032021 zao

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Vibrator
+import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener{
+class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClickListener, View.OnTouchListener{
     var counter:Int =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +18,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         setContentView(R.layout.activity_main)
         txv.setOnClickListener(this)
         img.setOnClickListener(this)
-        btn4.setOnClickListener(this)
+       // btn4.setOnClickListener(this)
+
+        btn4.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                counter = (0..100).random()
+                txv.text =counter.toString()
+            }
+        })
+
+        txv.setOnLongClickListener(object:View.OnLongClickListener{
+            override fun onLongClick(v: View?): Boolean {
+                counter+=2
+                txv.text =counter.toString()
+                return true
+            }
+        })
+
+        txv2.setOnTouchListener(this)
+
     }
     fun ADD(v: View){
         if(v == btn1){
@@ -43,6 +64,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
             txv.text =counter.toString()
         }
         txv.text =counter.toString()
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        val vibrator=getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if(event?.action ==MotionEvent.ACTION_DOWN){
+            txv2.text="on"
+        }
+        else if(event?.action ==MotionEvent.ACTION_UP){
+            txv2.text="off"
+        }
+        return true
     }
 }
 
